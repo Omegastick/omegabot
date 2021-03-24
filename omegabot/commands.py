93439bd2de734a556f06ebbf68816c0bc15fc34b
Omegastick <isaac@poulton.dev>
2021-03-24 17:50:15 +0000

    Add leaderboard command

--- omegabot/commands.py
+++ omegabot/commands.py
@@ -1,9 +1,16 @@
+import logging
+from typing import List
+
+from discord import Embed
 from discord import User as DiscordUser
 from discord.ext.commands import Context
 
 from omegabot.app import bot
+from omegabot.models import User
 from omegabot.services.points import add_points
 from omegabot.services.user import get_or_create_user
+
+LOG = logging.getLogger(__name__)
 
 
 @bot.command()
@@ -12,3 +19,13 @@ async def give(ctx: Context, discord_user: DiscordUser, amount: int):
     target_user = get_or_create_user(discord_user)
     target_user = add_points(command_user, target_user, amount)
     await ctx.send(f"Giving {discord_user.mention} {amount} points. They now have {target_user.points} points.")
+
+
+@bot.command()
+async def leaderboard(ctx: Context):
+    LOG.info("Printing leaderboard")
+    users: List[User] = User.select().order_by(User.points.desc())
+    embed = Embed()
+    embed.title = "Points Leaderboard"
+    embed.description = "\n".join([f"{user.name} - {user.points}" for user in users])
+    await ctx.send(embed=embed)
--- omegabot/event_handlers.py
+++ omegabot/event_handlers.py
@@ -1,5 +1,7 @@
 import logging
 
+from discord import Message
+
 from omegabot.app import bot
 
 LOG = logging.getLogger(__name__)
@@ -10,3 +12,9 @@ async def on_ready():
     LOG.info("Connected to Discord")
     LOG.info(f"Connected to guilds: {[guild.name for guild in bot.guilds]}")
     LOG.info(f"Bot name: {bot.user.name}")
+
+
+@bot.event
+async def on_message(message: Message):
+    if message.author.name == "Salaah01":
+        await message.channel.send("I am beyond your commands")
